301ee3fbc20048a5f8614518fc290028659defca
Marco Dias Lopes <rhino@pacodacomenda.com>
2021-09-19 19:23:43 +0200

    ci(actions): fixes issue with listr that requires an error when resolving

--- .actions/push.js
+++ .actions/push.js
@@ -1,41 +1,40 @@
 /**
  * Script to run before release
  */
- const shell = require('shelljs')
- const sugar = require('sugar-chalk')
- const Runner = require('listr')
+const shell = require('shelljs')
+const sugar = require('sugar-chalk')
+const Runner = require('listr')
  
- sugar.info('Creating new release')
+sugar.info('Creating new release')
  
- /**
-  * Handle promosise and log progress
-  * @param  {String} cmd command to run
-  * @param  {Object} log Ora instance to log information
-  * @return void
-  */
- function promise (cmd, ctx) {
-   return new Promise((resolve, reject) => {
-     shell.exec(cmd, { silent: true }, (code) => {
-       if (code) {
-         reject(code)
-         ctx.rejected = true
-       } else {
-         resolve()
-       }
-     })
-   })
- }
+/**
+ * Handle promosise and log progress
+ * @param  {String} cmd command to run
+ * @param  {Object} log Ora instance to log information
+ * @return void
+ */
+function promise (cmd) {
+  return new Promise((resolve, reject) => {
+    shell.exec(cmd, { silent: true }, (code) => {
+      if (code) {
+        reject(new Error('Command exited with code' + code))
+      } else {
+        resolve()
+      }
+    })
+  })
+}
  
- const tasks = new Runner([
-   { title: 'Check for vulnerabilites in dependencies', task: (ctx) => promise('yarn audit --groups dependencies', ctx) },
-   { title: 'Run unit tests', task: (ctx) => promise('yarn test', ctx) }
- ])
- 
- tasks.run()
-   .then(() => {
-     sugar.pass('Everything is ok')
-   })
-   .catch((error) => {
-     sugar.fail('Please fix the issues before pushing', error)
-   })
+const tasks = new Runner([
+  { title: 'Check for vulnerabilites in dependencies', task: () => promise('yarn audit --groups dependencies') },
+  { title: 'Run unit tests', task: () => promise('yarn test') }
+])
+
+tasks.run()
+  .then(() => {
+    sugar.pass('Everything is ok')
+  })
+  .catch((error) => {
+    sugar.fail('Please fix the issues before pushing')
+  })
  
